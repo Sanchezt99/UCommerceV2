4aaef6e6259c07b9c081c7e69db91ecf420e874d
Sanchezt99 <msanchezt@eafit.edu.co>
2020-05-13 21:31:17 -0500

    Vista Usuario arreglada

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -8,6 +8,8 @@ import {
   IonImg,
   IonButton,
   IonItemDivider,
+  IonList,
+  IonItem,
 } from "@ionic/react";
 /* import "./User.css"; */
 import './User.css'
@@ -38,25 +40,23 @@ const User: React.FC<RouteComponentProps> = (props) => {
             <IonImg src={profile.img} alt={profile.name} />
           </IonAvatar>
 
-          <IonItemDivider color='secondary'>
-            <IonLabel>Nombre: {profile.name}</IonLabel>
-          </IonItemDivider>
-
-          <IonItemDivider>
-            <IonLabel>Email: {profile.email}</IonLabel>
-          </IonItemDivider>
-
-          <IonItemDivider>
-            <IonLabel>Telefono: {profile.phone}</IonLabel>
-          </IonItemDivider>
-
-          <IonItemDivider>
-            <IonLabel>Género: {profile.gender}</IonLabel>
-          </IonItemDivider>
-
-          <IonItemDivider>
-            <IonLabel>Fecha de nacimiento: {profile.birthDate}</IonLabel>
-          </IonItemDivider>
+          <IonList color='secondary'>
+            <IonItem className='ion-margin' color='secondary'>
+              <IonLabel>Nombre: {profile.name}</IonLabel>
+            </IonItem>
+            <IonItem className='ion-margin' color='secondary'>
+              <IonLabel>Email: {profile.email}</IonLabel>
+            </IonItem>
+            <IonItem className='ion-margin' color='secondary'>
+              <IonLabel>Telefono: {profile.phone}</IonLabel>
+            </IonItem>
+            <IonItem className='ion-margin' color='secondary'>
+              <IonLabel>Género: {profile.gender}</IonLabel>
+            </IonItem>
+            <IonItem className='ion-margin' color='secondary'>
+              <IonLabel>Fecha de nacimiento: {profile.birthDate}</IonLabel>
+            </IonItem>
+          </IonList>
 
           <IonButton routerLink="/Add" color="primary" expand="block">
             Agregar producto o servicio nuevo
